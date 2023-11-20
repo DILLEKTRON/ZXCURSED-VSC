d4af3e74cb25a917e4694316aceea97cf46eb397
SADELS <57636464+DILLEKTRON@users.noreply.github.com>
2023-11-20 18:47:31 +0300

    Add files via upload

---
+++ slovari.py
@@ -0,0 +1,28 @@
+# Задание 1
+
+# input1 = input().split()
+# input2 = input().split()
+
+# list1 = list(map(int, input1))
+# list2 = list(map(int, input2))
+
+# set1 = set(list1)
+# set2 = set(list2)
+
+# elem= set1.intersection(set2)
+
+# result = sorted(list(elem))
+
+# print(*result)
+
+# Задача 2
+
+# with open('input.txt', 'r', encoding='utf-8') as file:
+#     text = file.read()
+
+# words = text.split()
+
+# unique_words = set(words)
+# count_unique_words = len(unique_words)
+
+# print(count_unique_words)
